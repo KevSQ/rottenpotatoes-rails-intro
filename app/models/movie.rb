ab7e0164d39b0182300e5f0aeb13fddd016ca8ff
Kevin Medina <km3628@columbia.edu>
2023-10-04 23:29:25 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+
+
+  def self.with_ratings(ratings_list)
+  return all if ratings_list.empty? || ratings_list == 0
+  where(rating: ratings_list)
+  end
+
+  def self.all_ratings
+    @ratings = ['G','PG','PG-13','R']
+  end
+
+
+  def self.ratings_to_show
+    @checked_ratings = Array.new()
+  end
+
 end
